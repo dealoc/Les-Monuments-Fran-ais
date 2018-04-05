3587492d1ddf271935d6c5fc3bff60c48c114de5
Dea-loC <dealoc@gmail.com>
2018-04-05 15:23:27 +0200

    Ajout JSON

---
+++ Les monuments français/Modele/Monument.swift
@@ -0,0 +1,17 @@
+//
+//  Monument.swift
+//  Les monuments français
+//
+//  Created by Dea-loC on 05/04/2018.
+//  Copyright © 2018 Dea-loC. All rights reserved.
+//
+
+import Foundation
+
+struct Monument : Decodable {
+    
+    var name: String?
+    var latitude: String?
+    var longitude: String?
+    
+}
